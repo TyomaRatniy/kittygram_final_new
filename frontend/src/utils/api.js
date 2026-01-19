04d8ff91a915a29d1fff9d4b62c766851a175d62
TyomaRatniy <tyomaratniy@gmail.com>
2026-01-19 04:39:10 +0300

    fix getUser export

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "/api";
+const BASE_URL = '/api';
 
 function request(url, options) {
   return fetch(url, options).then(checkResponse);
@@ -13,10 +13,10 @@ function checkResponse(res) {
 
 export const register = (email, password) => {
   return request(`${BASE_URL}/users/`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json"
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify({ email, password })
   });
@@ -24,23 +24,23 @@ export const register = (email, password) => {
 
 export const authorize = (email, password) => {
   return request(`${BASE_URL}/jwt/create/`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json"
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify({ email, password })
   });
 };
 
 export const getUser = () => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
   return request(`${BASE_URL}/users/me/`, {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${token}`
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
     }
   });
 };
